0914aabb75c0ccf005f4bff10b4bb295691a93ef
Jiang Bian <borderj@gmail.com>
2012-06-06 14:45:42 +0800

    gotour: exercise pic slice

---
+++ golang/gotour/picSlice.go
@@ -0,0 +1,19 @@
+package main
+
+import "bitbucket.org/mikespook/go-tour-zh/pic"
+
+func Pic(dx, dy int) [][]uint8 {
+	mat := make([][]uint8, dx)
+
+	for i := range mat {
+		mat[i] = make([]uint8, dy)
+		for j := range mat[i] {
+			mat[i][j] = uint8(i ^ j)
+		}
+	}
+	return mat
+}
+
+func main() {
+	pic.Show(Pic)
+}
