7bc4be3a8d8ba5ada7a340688518cf2415fd1b06
Jiang Bian <borderj@gmail.com>
2014-02-19 18:55:24 +0800

    threadgroup

--- CC++/boost/threadgroup.cpp
+++ CC++/boost/threadgroup.cpp
@@ -8,27 +8,37 @@ void wait(int seconds)
     boost::this_thread::sleep(boost::posix_time::seconds(seconds)); 
 } 
 
+int max = 29;
 int count = 0;
 boost::mutex mutex;
 
-void thread(int id) 
+void thread(int id, int start, int end) 
 { 
-    for (int i = 0; i < 500; ++i) 
+    printf("id: %d - start: %d, end: %d\n", id, start, end);
+    for (int i = start; i < end; ++i) 
     { 
         //wait(1); 
-        //printf("id: %d - %d\n", id, i);
+        printf("id: %d - %d\n", id, i);
         boost::unique_lock<boost::mutex> lock(mutex);
         ++count;
     }
 }
 
-int main() 
-{ 
-    int coreNum = boost::thread::hardware_concurrency();
-    //std::cout << "CPU Core Num: " << coreNum << std::endl;
+int main(int argc, char *argv[]) 
+{
+    if (argc == 2) {
+        max = atoi(argv[1]);
+    }
+    int coreNum = boost::thread::hardware_concurrency() - 1;
+    int per = max / coreNum;
+    printf("per: %d, coreNum: %d, max: %d\n", per, coreNum, max);
     boost::thread_group threads;
-    for (int i=0; i < coreNum * 12; i++) {
-        threads.create_thread(boost::bind(thread, i));
+    for (int i=0; i < coreNum; i++) {
+        if (i != coreNum -1) {
+            threads.create_thread(boost::bind(thread, i, i * per, (i+1) * per));
+        } else {
+            threads.create_thread(boost::bind(thread, i, i * per, max));
+        }
     }
     threads.join_all();
     printf("count: %d\n", count);
