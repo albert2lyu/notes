b365a7e114a0c024f8ddefa8e61efa93c0dfa8d4
Jiang Bian <borderj@gmail.com>
2014-04-02 17:26:31 +0800

    split

--- CCPP/string/splitBygetLine.cpp
+++ CCPP/string/splitBygetLine.cpp
@@ -2,6 +2,22 @@
 #include <iostream>
 #include <vector>
 
+void split(const std::string& src, const std::string separate_character, vector<string> &strs)  
+{
+    int separate_characterLen = separate_character.size();
+    int lastPosition = 0,index = -1;  
+    while (-1 != (index = src.find(separate_character,lastPosition)))  
+    {  
+        strs.push_back(src.substr(lastPosition,index - lastPosition));  
+        lastPosition = index + separate_characterLen;  
+    }  
+    std::string lastString = src.substr(lastPosition);
+    if (!lastString.empty()) {
+        strs.push_back(lastString);
+    }
+}
+
+
 int main() {
     std::vector<std::string> strings;
     std::istringstream f("denmark;sweden;india;us");
